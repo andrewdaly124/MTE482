615076a652a0c80e3c5910d60fadd01e9628ff1b
andrewdaly124@gmail.com <andrewdaly124@gmail.com>
2022-02-14 17:31:34 -0500

    back to windows pc, format norms and package lock. You can basically ignore this commit

--- Andrew/andrews-app/src/ui/index.jsx
+++ Andrew/andrews-app/src/ui/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { KeyDownHandler, KeyUpHandler } from '../utils/input_handler';
+import React from "react";
+import { KeyDownHandler, KeyUpHandler } from "../utils/input_handler";
 
-import MainMenu from './components/mainMenu';
-import Panel from './components/panel';
-import PresetEdit from './components/dialogs/presetEdit';
+import MainMenu from "./components/mainMenu";
+import Panel from "./components/panel";
+import PresetEdit from "./components/dialogs/presetEdit";
 
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 // Root UI component
 export default function Ui() {
   // Initialize input handlers - Nothing rn
-  document.addEventListener('keydown', KeyDownHandler);
-  document.addEventListener('keyup', KeyUpHandler);
+  document.addEventListener("keydown", KeyDownHandler);
+  document.addEventListener("keyup", KeyUpHandler);
 
   return (
     <div className={styles.ui}>
